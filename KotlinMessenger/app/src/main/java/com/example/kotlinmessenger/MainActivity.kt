ec65f3da54a36250a6ce1ff073ab9720c0131ef7
hika019 <ikazuti2001@gmail.com>
2020-10-03 00:03:54 +0900

    add Create User function

--- KotlinMessenger/app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
+++ KotlinMessenger/app/src/main/java/com/example/kotlinmessenger/MainActivity.kt
@@ -4,20 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        performRegister()
 
         register_button_register.setOnClickListener {
-            val email = email_edittext_register.text.toString()
-            val password = password_edittext_registaer.text.toString()
-
-            Log.d("MainActivity", "Email is: "+ email)
-            Log.d("MainActivity", "Password is: $password")
 
 
 
@@ -31,5 +28,32 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun performRegister(){
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_registaer.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Email is: "+ email)
+        Log.d("MainActivity", "Password is: $password")
+
+
+        //create a user email and password by Firebase Authentication
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(it.isSuccessful) return@addOnCompleteListener
+
+                //else if successful
+
+                Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failed to create user: ${it.message}")
+            }
     }
 }
